7343c993e21fa5bd9ff7ff1ef184c48ae05395ce
Pacmanzzz <71653017+Pacmanzzz@users.noreply.github.com>
2022-08-05 09:19:01 +0700

    Add files via upload

---
+++ src/components/ChannelTools/channelTools.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../CSS/channelTools.css"
+
+const ChannelTools = () => {
+    return (
+        <>
+            <div className="channel_tools">
+                <div className="new_channel_tool">+Chats</div>
+                <div className="channel_tool_action">Thao tác</div>
+            </div>
+        </>
+    )
+}
+
+export default ChannelTools;
